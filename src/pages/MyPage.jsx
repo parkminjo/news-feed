924f8b6d1435643e34dc72fa110a6fcaf925e8eb
woozizi <c980805@naver.com>
2025-02-13 23:06:10 +0900

    test : db체크

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,5 +1,79 @@
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { supabase } from '../services/supabaseClient';
+
+const MyPage = () => {
+  const [userData, setUserData] = useState([]);
+  // const navigate = useNavigate();
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const { data, error } = await supabase.from('test1').select('*');
+        if (error) throw error;
+        setUserData(data);
+      } catch (error) {
+        console.error('데이터 불러오기 실패:', error.message);
+      }
+    };
+    getUserData();
+
+    return;
+  }, []);
+
+  const handleGotoFollowerList = () => {
+    //팔로워 목록 페이지로 이동이거나 모달 열기
+    alert('모달로 이동');
+    // navigate();
+  };
+
+  const handleGotoFollowingList = () => {
+    //팔로워 목록 페이지로 이동이거나 모달 열기
+    alert('모달로 이동');
+    // navigate();
+  };
+
+  const handleGotoPostingList = () => {
+    //게시물 목록으로 이동
+    alert('모달로 이동');
+    // navigate();
+  };
+
+  const handleGotoDetailPage = () => {
+    //게시물 디테일로 이동
+    alert('게시물 디테일페이지로 이동');
+    // navigate();
+  };
+
+  //임시 게시글
+  const posts = Array.from({ length: 30 }, (_, index) => index + 1);
+
+  return (
+    <ProfileContainer>
+      <ProfileHeader>
+        <ProfileImage src="" alt="" />
+        <ProfileInfo>
+          <NickName>{userData.length > 0 ? userData[0].nick_name : 'Loading...'}</NickName>
+          <ProfileStats>
+            <li onClick={handleGotoPostingList}>게시물 1</li>
+            <li onClick={handleGotoFollowerList}>팔로워 1</li>
+            <li onClick={handleGotoFollowingList}>팔로잉 1</li>
+          </ProfileStats>
+        </ProfileInfo>
+      </ProfileHeader>
+      <PostGrid>
+        {posts.map((post) => (
+          <FeedPost onClick={handleGotoDetailPage} key={post}>
+            게시물
+          </FeedPost>
+        ))}
+      </PostGrid>
+    </ProfileContainer>
+  );
+};
+
+export default MyPage;
 
 const ProfileContainer = styled.div`
   display: flex;
@@ -48,6 +122,7 @@ const ProfileStats = styled.ul`
   li {
     margin-right: 20px;
     font-size: medium;
+    cursor: pointer;
   }
 `;
 
@@ -60,39 +135,9 @@ const PostGrid = styled.section`
   margin: 40px auto;
 `;
 
-const PostItem = styled.div`
+const FeedPost = styled.div`
   border: 1px solid black;
   padding: 10px;
   text-align: center;
   height: 150px;
 `;
-
-const MyPage = () => {
-  useEffect(() => {});
-
-  return (
-    <ProfileContainer>
-      <ProfileHeader>
-        <ProfileImage src="" alt="" />
-        <ProfileInfo>
-          <NickName>NickName</NickName>
-          <ProfileStats>
-            <li>게시물 1</li>
-            <li>팔로워 1</li>
-            <li>팔로잉 1</li>
-          </ProfileStats>
-        </ProfileInfo>
-      </ProfileHeader>
-      <PostGrid>
-        <PostItem>게시물1</PostItem>
-        <PostItem>게시물2</PostItem>
-        <PostItem>게시물3</PostItem>
-        <PostItem>게시물4</PostItem>
-        <PostItem>게시물5</PostItem>
-        <PostItem>게시물6</PostItem>
-      </PostGrid>
-    </ProfileContainer>
-  );
-};
-
-export default MyPage;
