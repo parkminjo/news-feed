cc4a774331fd0e797880df468cb6470364ccd7f8
Minjo Park <consistent.312@gmail.com>
2025-02-13 11:07:29 +0900

    chore: AuthProvider 위치 main으로 이동

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,23 +1,20 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
-import SignUp from '../pages/SignUp';
 import MyPage from '../pages/MyPage';
 import Search from '../pages/Search';
-import { AuthProvider } from '../context/AuthProvider';
+import SignUp from '../pages/SignUp';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/search" element={<Search />} />
-        </Routes>
-      </AuthProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
     </BrowserRouter>
   );
 };
