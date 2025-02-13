9c42bf61136b071ab30bbfec42ca74aa213948ea
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-02-13 18:54:02 +0900

    design: header 컴포넌트 기본 디자인 구현

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,5 +1,55 @@
+import styled from 'styled-components';
+import { fontSize } from '../../styles/fontSize';
+import { CenterWrapper } from '../../styles/GlobalStyle';
+import { FaRegBell, FaUserCircle } from 'react-icons/fa';
+
+const StContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 8px 16px;
+  font-size: ${fontSize.medium};
+`;
+
+const StLogo = styled.div`
+  font-weight: bold;
+`;
+
+const StIconsWrapper = styled(CenterWrapper)`
+  gap: 24px;
+`;
+
+const StIconWrapper = styled(CenterWrapper)`
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+const StBellIcon = styled(FaRegBell)`
+  color: black;
+`;
+
+const StPlusIcon = styled(FaUserCircle)`
+  color: gray;
+  margin-right: 10px;
+`;
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <StContainer>
+      <StLogo>로고</StLogo>
+      <StIconsWrapper>
+        <StIconWrapper>
+          <StBellIcon size={30} />
+        </StIconWrapper>
+        <StIconWrapper>
+          <StPlusIcon size={30} />
+        </StIconWrapper>
+      </StIconsWrapper>
+    </StContainer>
+  );
 };
 
 export default Header;
