a019c54d46e6f3b661f8a7c1d310ac21f4d15751
Minjo Park <consistent.312@gmail.com>
2025-02-13 14:21:47 +0900

    design: 공통 색깔, 폰트 사이즈 설정

---
+++ src/styles/color.js
@@ -0,0 +1,6 @@
+export const color = {
+  main: '#ff6903',
+  gray: '#ACB5BD',
+  black: '#121212',
+  white: '#FFFFFF'
+};
---
+++ src/styles/fontSize.js
@@ -0,0 +1,6 @@
+export const fontSize = {
+  small: '12px',
+  medium: '16px',
+  large: '24px',
+  xLarge: '32px'
+};
