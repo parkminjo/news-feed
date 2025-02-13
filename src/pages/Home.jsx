8a008fb75b1a0817f4d6e8e4b9968abd62016543
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-02-13 18:03:31 +0900

    design: 표시확인용으로 Home 화면에 header, sidebar, footer를 포함한 레이아웃 디자인
    - 레이아웃 상단에 Header 컴포넌트 배치
    - 메인 컨텐츠 좌측에 Sidebar 컴포넌트 배치
    - 레이아웃 하단에 Footer 컴포넌트 배치

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,41 @@
+import Header from '../components/layout/Header';
+import Sidebar from '../components/layout/SideBar';
+import styled from 'styled-components';
+
+const StContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StMainContent = styled.main`
+  display: flex;
+  flex: 1;
+`;
+
+const StContentWrapper = styled.div`
+  padding: 10px;
+`;
+
+const StFooter = styled.footer`
+  background-color: black;
+  color: white;
+  padding: 10px;
+  text-align: center;
+`;
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <StContainer>
+      <Header />
+      <StMainContent>
+        <Sidebar />
+        <StContentWrapper>
+          <div>Home</div>
+        </StContentWrapper>
+      </StMainContent>
+      <StFooter>Footer</StFooter>
+    </StContainer>
+  );
 };
 
 export default Home;
