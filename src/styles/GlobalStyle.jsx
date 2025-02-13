f2187d4dc59c4e206b95a61025ebe4f5e0fc0ad4
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-02-13 17:46:01 +0900

    design: GlobalStyle 추가 및 적용

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -1,6 +1,19 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
+import styled from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   ${reset};
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+export const CenterWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
