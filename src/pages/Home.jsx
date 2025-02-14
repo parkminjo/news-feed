02d2f4136c7ff81980b2d434c2985ae19eb9be7f
ye21iin <kathykr120@gmail.com>
2025-02-14 18:17:03 +0900

    Feat: 렌더링 기능 구현 / UI 요소 수정(width)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,39 @@
 import Header from '../components/layout/Header';
 import Sidebar from '../components/layout/SideBar';
 import styled from 'styled-components';
+import { supabase } from '../services/supabaseClient';
+import { useEffect, useState } from 'react';
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  // supabase - SELECT 함수 (getPost)
+  const getPost = async () => {
+    try {
+      const { data } = await supabase.from('posts').select();
+      setPosts(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getPost();
+  }, []); // 초기 렌더링 시에만 데이터 fetch
+
   return (
     <StContainer>
       <Header />
       <StMainContent>
         <Sidebar />
         <StContentWrapper>
-          <div>Home</div>
+          <StPostWrapper>
+            {posts.map((post) => (
+              <div style={{ border: '1px solid black' }} key={post.id}>
+                <p>{post.title}</p>
+              </div>
+            ))}
+          </StPostWrapper>
         </StContentWrapper>
       </StMainContent>
     </StContainer>
@@ -31,4 +55,12 @@ const StMainContent = styled.main`
 
 const StContentWrapper = styled.div`
   padding: 10px;
+  width: 100%;
+`;
+
+const StPostWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
