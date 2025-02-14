2a0bbbe1167651a78d1db19c53018a8004ac74db
siheon <rlatlgjs357@gmail.com>
2025-02-14 11:08:57 +0900

    feat: toggle, scroll 기능 추가

--- src/components/modals/BookMarkModal.jsx
+++ src/components/modals/BookMarkModal.jsx
@@ -1,21 +1,47 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import { color } from '../../styles/color';
 import { fontSize } from '../../styles/fontSize';
 
+const img = [
+  '../../public/img/LoginCat.png',
+  '../../public/img/LoginCat.png',
+  '../../public/img/LoginCat.png',
+  '../../public/img/LoginCat.png',
+  '../../public/img/LoginCat.png',
+  '../../public/img/LoginCat.png',
+  '../../public/img/LoginCat.png',
+  '../../public/img/LoginCat.png',
+  '../../public/img/LoginCat.png',
+  '../../public/img/LoginCat.png',
+  '../../public/img/LoginCat.png',
+  '../../public/img/LoginCat.png',
+];
 
 const BookMarkModal = ({ onClose }) => {
+  const [selectedPost, setSelectedPost] = useState([]);
+
+  const togglePost = (src) => {
+    setSelectedPost((prev) => {
+      prev.includes(src) ? prev.filter((item) => item !== src) : [...prev, src];
+    });
+  };
+
   return (
     <StContainer>
       <StModal>
-        <StCloseButton onClick={onClose}>
-          &lt; 저장됨
-        </StCloseButton>
-        <img src='' alt='PostIMG' />
-        <img src='' alt='PostIMG' />
-        <img src='' alt='PostIMG' />
-        <img src='' alt='PostIMG' />
-        <img src='' alt='PostIMG' />
-        <img src='' alt='PostIMG' />
+        <StCloseButton onClick={onClose}>&lt; 저장됨</StCloseButton>
+        <StImgGrid>
+          {img.map((src, index) => (
+            <StCatImg
+              key={index}
+              src={src}
+              alt="postImg"
+              onClick={() => togglePost(src)}
+              seleted={selectedPost.includes(src)}
+            />
+          ))}
+        </StImgGrid>
       </StModal>
     </StContainer>
   );
@@ -37,11 +63,17 @@ const StContainer = styled.div`
 
 const StModal = styled.div`
   background-color: ${color.white};
+  width: 70%;
+  height: 90%;
   padding: 20px;
   border-radius: 8px;
-  width: 300px;
-  text-align: center;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow-y: auto;
+  max-height: 100%;
 `;
 
 const StCloseButton = styled.button`
@@ -56,5 +88,27 @@ const StCloseButton = styled.button`
 
   &:hover {
     color: ${color.black};
+  }
+`;
+
+const StImgGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+  width: 100%;
+  max-width: 600px;
+  margin-top: 50px;
+`;
+
+const StCatImg = styled.img`
+  width: 100%;
+  height: auto;
+  cursor: pointer;
+  border-radius: 5px;
+  transition: transform 0.2s, border 0.2s;
+  border: ${(props) => (props.selected ? `3px solid ${color.main}` : 'none')};
+
+  &:hover {
+    transform: scale(1.05);
   }
 `;
