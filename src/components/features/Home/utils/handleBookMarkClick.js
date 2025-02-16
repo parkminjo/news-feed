b5f04e8f6295a84fbebfc6b4158f31c758eafaa4
Minjo Park <consistent.312@gmail.com>
2025-02-17 01:17:07 +0900

    feat: 북마크 추가/취소 함수 구현

---
+++ src/components/features/Home/utils/handleBookMarkClick.js
@@ -0,0 +1,36 @@
+import { supabase } from '../../../../services/supabaseClient';
+
+/** 북마크 추가/취소 함수 */
+export const handleBookMarkClick = async (isLogin, isBookMarkClicked, setIsBookMarkClicked, user, post) => {
+  if (!isLogin) return; // 로그인하지 않은 유저 좋아요 기능 비활성화
+
+  // 좋아요 취소
+  if (isBookMarkClicked) {
+    try {
+      const { error } = await supabase.from('bookmarks').delete().eq('send_user_id', user.id).eq('post_id', post.id);
+
+      if (error) {
+        throw error;
+      }
+
+      setIsBookMarkClicked(false);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // 좋아요 추가
+  if (!isBookMarkClicked) {
+    try {
+      const { error } = await supabase.from('bookmarks').insert([{ send_user_id: user.id, post_id: post.id }]);
+
+      if (error) {
+        throw error;
+      }
+
+      setIsBookMarkClicked(true);
+    } catch (error) {
+      console.error('북마크 처리 오류:', error);
+    }
+  }
+};
