c7a5a70b3ec378bd0466599c4e8266643636eebd
Sumin-Lee12 <leesumin041@gmail.com>
2025-02-16 15:46:54 +0900

    feature: 모달창 '닫기'와 '제출'버튼 클릭 시 이미지 초기화 구현 완료와 supabase연동 로직 작성 중

--- src/components/modals/PostCreateModal.jsx
+++ src/components/modals/PostCreateModal.jsx
@@ -2,23 +2,123 @@ import styled from 'styled-components';
 import { color } from '../../styles/color';
 import { fontSize } from '../../styles/fontSize';
 import { useRef, useState } from 'react';
+import React, { useEffect } from 'react';
+import { supabase } from '../../services/supabaseClient';
 
 const PostCreateModal = ({ isOpen, close }) => {
   const [imgPreview, setImagePreview] = useState(''); // img 파일이 imgPreview 상태에 Base64 문자열로 들어갈 것이기 때문에 기본값이 ""임
   const imgRef = useRef(null); // useRef는 컴포넌트 안에서 DOM 요소나 값을 직접 참조!! ==> useState()를 사용할 때와 달리 input태그의 값에 직접 접근할 수 있어 불필요한 리렌더링을 줄일 수 있음
+  const [posts, setPosts] = useState([]);
+  const [post, setPost] = useState({ title: '', content: '', tags: '', img: '' });
+  const [title, setTitle] = useState('');
+  const [contents, setContents] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [tags, setTags] = useState(''); // tag도 useState로 하면 되는 게 맞나....?
 
-  if (!isOpen) {
-    return null; // isOpen이 false면 모달창을 렌더링하지 않음
-  }
+  useEffect(() => {
+    // supabase에서 posts 테이블 데이터를 가져오는 함수 (***실패할 수도 있는 요청이니 try catch로 감싸기***)
+    const getPosts = async () => {
+      try {
+        const { data, error } = await supabase.from('posts').select('*'); //외부(supabase)에서 데이터를 가져옴 (posts테이블에 있는 데이터 모두)
+        if (error) throw error;
+        setPosts(data); // 가져온 데이터를 setPosts에 넣으므로서 외부데이터를 내부에서 관리 가능한 상태로 만듬
+        console.log('데이터 확인 ==>', data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    // 2. 실행
+    getPosts();
+  }, []);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert('폼이 제출되었습니다!'); 
-    close();
+  // postImage업로드 함수
+  const handleImgUpload = async (e) => {
+    const postImgFile = e.target.files[0];
+    const fileName = `${Date.now()}_${postImgFile.name}`; // 고유한 파일 이름을 만들기 위해 시간을 추가함
+    // supabase storage에 파일 업로드
+    const { data: uploadData, error: uploadError } = await supabase.storage
+      .from('my-bucket-name')
+      .upload(fileName, postImgFile);
+    // 에러 발생 시 실행될 기능
+    if (uploadError) {
+      console.log('포스트 이미지 파일 업로드 중 에러 발생 ==> ', uploadError);
+      return;
+    }
+    // 업로드한 이미지의 public URL 가져오기
+    const { publicURL, error: urlError } = supabase.storage.from('my-bucket-name').getPublicUrl(fileName); // 이전에 만들어둔 my-bucket-name이름의 버켓에서 이미지 파일의 publicUrl을 가져온다
+    // 파일 url 가져오기 중 에러 발생 시 실행
+    if (urlError) {
+      console.log('포스트 이미지 파일 url 가져오기 중 에러 발생 ==> ', urlError);
+      return;
+    }
+    // 이미지 url을 posts 테이블에 저장
+    const { data: postData, error: postError } = await supabase.from('posts').insert([
+      {
+        title,
+        content: contents,
+        img: publicURL
+      }
+    ]);
+    // 에러 발생 시 실행될 기능
+    if (postError) {
+      console.log('posts테이블에 이미지 url추가 중 에러 발생 ==> ', postError);
+    } else {
+      if (postData.length > 0) {
+        setPost((prev) => ({
+          ...prev,
+          img: postData[0].img
+        }));
+      }
+    }
   };
 
-  const handleImgPreview = () => {
-    const imgFile = imgRef.current.files[0]; // 1) imgRef.current는 input요소를 참조 => files는 그 input에서 선택한 파일들 => files[0]는 선택된 첫 번째 파일(preview용)
+  // 이 함수를 실행할 떄 status: 401(unauthorized)이 나오면 RLS 관련 문제
+  const handleAddPost = async (e) => {
+    e.preventDefault();
+
+    const { data: sessionData } = await supabase.auth.getSession(); // supabase에서 세션정보(현재 로그인한 유저의 정보)를 받아와서 그 데이터를 sessionData라는 이름의 변수로 저장
+    const user = sessionData?.session?.user; // getSession에 있는 data라는 객체안에 있는 session이라 객체안에서 user라는 키워드의 값을 가져와 user라는 이름의 변수로 지정 / ?. <== 이거는 Optional Chaining란 것으로 속성이 없을 떄 undefined를 반환해 오류가 발생하지 않게 함!
+
+    const { data, error } = await supabase.from('posts').insert(post).select(); // 해석: .from('posts') = posts라는 이름의 테이블에서 -> .insert(post) = post라는 이름으로 새로운 게시물 추가 -> .select() = 그렇게 추가한 데이터를 다시 가져옴 & data에 넣음 / 여기서 post는 데이터베이스에 추가될 데이터를 담고 있는 자바스크립트 객체의 이름 / .insert대신 비교하고 업데이트와 추가 둘 중 하나를 실행하는 .upsert도 있지만 게시글 추가는 말 그대로 "추가"를 하는 기능이라 보고 .insert를 사용했다.
+
+    if (error) {
+      console.log('게시글 추가 중 에러 발생 => ', error);
+      return; // return을 사용하여 에러 발생 시 함수를 종료시킨다.
+    }
+
+    setPosts((prev) => [
+      //여기에 동기화 로직!을 만들어서 supabase테이블에도 들어가는 동시에 화면에도 렌더링 되게 구현
+      ...prev, // ...prev, {...} 의 의미 ==> 기존의 게시글 목록은 그대로 유지하면서, 새로운 게시글을 추가한다는 것!
+      {
+        id: data[0].id, // id는 supabase가 자동으로 만들어준다!
+        created_at: data[0].created_at,
+        writer_id: user?.id, // session으로 가져오도록 바꿔주기
+        title: data[0].title,
+        content: data[0].content,
+        img: data[0].img,
+        tag: data[0].tags
+      }
+    ]);
+  };
+
+  // isOpen이 true일 때만 모달창을 여는 함수
+  if (!isOpen) {
+    return null;
+  }
+  // 게시글 제출 함수
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('폼이 제출되었습니다!');
+    handleResetImgPreview();
+    close();
+  };
+  // 이미지 미리보기 함수
+  const handleImgPreview = (e) => {
+    const imgFile = e.target.files[0]; // 1) imgRef.current는 input요소를 참조 => files는 그 input에서 선택한 파일들 => files[0]는 선택된 첫 번째 파일(preview용)
+    if (!imgFile) {
+      // 이미지 파일이 없으면 함수를 종료
+      return;
+    }
     const reader = new FileReader(); // 2) FileReader 객체 생성 => new FileReader는 컴퓨터에게  "input에 들어가는 파일을 읽어서 JavaScript에서 사용할 수 있는 형태로 변환해줘!"라고 말하는 객체
     reader.readAsDataURL(imgFile); // 3) .readAsDataURL은 이제 읽는 방식을 "JavaScript에서 사용할 수 있는 형태인 Base64 형식으로 변환해줘!"라고 말하는 거
     reader.onloadend = () => {
@@ -27,38 +127,52 @@ const PostCreateModal = ({ isOpen, close }) => {
     };
   };
 
+  const handleResetImgPreview = () => {
+    setImagePreview('');
+  };
+
   return (
     <StOverlay>
       <StContainer>
         <StForm onSubmit={handleSubmit}>
           <h2>모달창</h2>
-          <StCloseButton type="button" onClick={close}>
+          <StCloseButton
+            type="button"
+            onClick={() => {
+              close();
+              handleResetImgPreview();
+            }}
+          >
             닫기
           </StCloseButton>
           <StInputWrapper>
             <Stdiv imgPreview={imgPreview}>
               <label htmlFor="postImage">이미지 추가</label>
-              <img src={imgPreview ? imgPreview : null} multiple alt='이미지 미리보기 실패'/>
+              <img src={imgPreview ? imgPreview : null} multiple alt="이미지 미리보기 실패" />
               <input
                 type="file"
                 accept="image/*, video/*"
                 id="postImage"
-                onChange={handleImgPreview}
+                onChange={(e) => {
+                  setImgUrl(e.target.value);
+                  handleImgPreview(e);
+                }}
                 ref={imgRef}
+                required
               />
             </Stdiv>
             <StTextInputWrapper>
               <StLabel>
                 제목
-                <input type="textarea" required />
+                <input type="text" required onChange={(e) => setTitle(e.target.value)} />
               </StLabel>
               <StLabel>
                 내용
-                <textarea required></textarea>
+                <textarea required onChange={(e) => setContents(e.target.value)}></textarea>
               </StLabel>
               <StLabel>
                 태그
-                <input type="text" required />
+                <input type="text" required onChange={(e) => setTags(e.target.value)} />
               </StLabel>
             </StTextInputWrapper>
           </StInputWrapper>
@@ -119,7 +233,7 @@ const StForm = styled.form`
   flex-direction: column;
 
   h2 {
-    margin-top: 50px;
+    margin: 25px;
     color: ${color.main};
   }
 `;
@@ -186,7 +300,7 @@ const StLabel = styled.label`
   font-weight: 700;
 
   input {
-    width: 28vw;
+    width: 30vw;
     border: 1px solid black;
     border-radius: 10px;
     padding: 10px;
